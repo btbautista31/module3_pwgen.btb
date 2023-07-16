92df518e18c2737731d57a1716cd1640f4909213
btbautista31 <134978501+btbautista31@users.noreply.github.com>
2023-07-16 07:02:53 +0100

    Update javascript to fix for loop that populates random characters to the correct lengthh

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -22,54 +22,80 @@ function generatePassword () {
     console.log("Special Characters: " + pwSpecial);
     
     //Object for inputs/prompts
-    var keys = {
-      upperCase: ["A","B","C","D"],
-      lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-      number: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-      special: ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"],
+    // var keys = {
+      var upperCase = ["A","B","C","D"];
+      var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+      var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+      var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+    // }
+
+  //   function generatedPassword() {
+  //     let createdPassword = [];
+  //     for(let i=0; i<pwLength; i++)
+  //     {
+  //       if (pwUpper == true) {
+  //         var genChar = keys.upperCase[Math.floor(Math.random() * keys.upperCase.length)];
+  //         createdPassword.push(genChar);
+  //         console.log('Upper: ' + genChar);
+  //         // var genChar = keys.lowerCase[Math.floor(Math.random() * keys.lowerCase.length)];
+  //         // createdPassword.push(genChar);
+  //         // console.log('Lower: ' + genChar);
+  //         // var genChar = keys.number[Math.floor(Math.random() * keys.number.length)];
+  //         // createdPassword.push(genChar);
+  //         // console.log('Number: ' + genChar);
+  //         // var genChar = keys.special[Math.floor(Math.random() * keys.special.length)];
+  //         // createdPassword.push(genChar);  
+  //         // console.log('Special: ' + genChar);
+  //         }
+  //         if (pwLower == true) {
+  //         var genChar = keys.lowerCase[Math.floor(Math.random() * keys.lowerCase.length)];
+  //         createdPassword.push(genChar);
+  //         console.log('Lower: ' + genChar);
+  //         }
+
+  //     //   else if (pwNumber == true) {
+  //     //     var genChar = keys.number[Math.floor(Math.random() * keys.number.length)];
+  //     //     createdPassword.push(genChar);
+  //     //     console.log('Number: ' + genChar);
+  //     //       }
+  //     //   else if (pwSpecial == true) {
+  //     //     var genChar = keys.special[Math.floor(Math.random() * keys.special.length)];
+  //     //     createdPassword.push(genChar);  
+  //     //     console.log('Special: ' + genChar);
+  //     // }
+  //   }
+  //   console.log("my password: " + createdPassword);
+  // }
+
+  var passwordCharacters = []
+  if (pwSpecial) {passwordCharacters = passwordCharacters.concat(special)}
+  if (pwNumber) {passwordCharacters = passwordCharacters.concat(number)}
+  if (pwLower) {passwordCharacters = passwordCharacters.concat(lowerCase)}
+  if (pwUpper) {passwordCharacters = passwordCharacters.concat(upperCase)}
+
+  console.log(passwordCharacters)
+
+    // Empty string to be filled based on for loop selecting random characters from the array
+    var randomPassword = ""
+    for (var i = 0; i < pwLength; i++) {
+      randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+      console.log(randomPassword)
     }
-
-    
-    function generatedPassword() {
-      let createdPassword = [];
-      for(let i=0; i<pwLength; i++)
-      {
-        if (pwUpper == true) {
-          var genChar = keys.upperCase[Math.floor(Math.random() * keys.upperCase.length)];
-          createdPassword.push(genChar);
-          console.log('Upper: ' + genChar);
-          }
-  
-        else if (pwLower == true) {
-          var genChar = keys.lowerCase[Math.floor(Math.random() * keys.lowerCase.length)];
-          createdPassword.push(createdPassword*genChar);
-          console.log('Lower: ' + genChar);
-          }
-        else if (pwNumber == true) {
-          var genChar = keys.number[Math.floor(Math.random() * keys.number.length)];
-          createdPassword.push(genChar);
-          console.log('Number: ' + genChar);
-            }
-        else if (pwSpecial == true) {
-          var genChar = keys.special[Math.floor(Math.random() * keys.special.length)];
-          createdPassword.push(genChar);  
-          console.log('Special: ' + genChar);
-      }
-    }
-    console.log(createdPassword);
-  }
-generatedPassword();
+    return randomPassword;
 
 
-    
+
+
+
+    generatedPassword();
   //Validate inputs
   //Generate password
   //Display password
-  return "Generated Password here";
+    return "Generated Password here";
 
 }
-
 generatePassword();
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -79,6 +105,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-//Reference Code
