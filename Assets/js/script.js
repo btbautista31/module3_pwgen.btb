970c96c4ff0283ca85a31f5628b307962e684fdb
btbautista31 <134978501+btbautista31@users.noreply.github.com>
2023-07-16 08:00:47 +0100

    Added validation check for password length and type coercion

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,29 +5,87 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword () {
   console.log("Button clicked!");
   //Prompts
-    //Prompt for length
-      var pwLength = prompt("Enter password length: ");
+      //Prompt for length
+      for (var s = 0; s < 1; s+0) {
+      var pwLength = prompt("Enter password length(between 8 and 128): ");     
+        if (pwLength <= 8 || pwLength >= 128) {
+          alert("Length must be between 8 and 128. Please try again");
+          }
+        else if (pwLength >= 8 || pwLength <= 128) {
+          console.log("Password length is good.");
+          s++;
+            }
+        else {
+          alert("Length cannot be a letter. Please choose a number between 8 and 128.");
+        }
+      }
+
       console.log("Password Length: " + pwLength);
-    //Prompt for casing
+
+      //Prompt for casing
       var pwUpper = confirm("Include Upper Case?: Ok = Yes or Cancel = No");
       console.log("Upper Case: " + pwUpper)
       var pwLower = confirm("Include Lower Case?: Ok = Yes or Cancel = No");
       console.log("Lower Case: " + pwLower);
-    //Prompt for numbers
+      //Prompt for numbers
       var pwNumber = confirm("Include Numbers?: Ok = Yes or Cancel = No");
       console.log("Numbers: " + pwNumber);
+      //Prompt for special characters
+      var pwSpecial = confirm("Include Special Characters?: Ok = Yes or Cancel = No");
+      console.log("Special Characters: " + pwSpecial);
 
-    //Prompt for special characters
-    var pwSpecial = confirm("Include Special Characters?: Ok = Yes or Cancel = No");
-    console.log("Special Characters: " + pwSpecial);
-    
-    //Object for inputs/prompts
-    // var keys = {
-      var upperCase = ["A","B","C","D"];
+      //Character container
+      var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
       var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
       var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-      var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-    // }
+      var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "/", "?", "<", ">"];
+
+
+  //Random character generator
+      var pwChars = []
+      if (pwSpecial) {pwChars = pwChars.concat(special)}
+      if (pwNumber) {pwChars = pwChars.concat(number)}
+      if (pwLower) {pwChars = pwChars.concat(lowerCase)}
+      if (pwUpper) {pwChars = pwChars.concat(upperCase)}
+
+  //For loop to generate password  
+      var generatedPasword = ""
+        for (var i = 0; i < pwLength; i++) {
+        generatedPasword = generatedPasword + pwChars[Math.floor(Math.random() * pwChars.length)];
+        console.log(generatedPasword)
+        }
+
+  return generatedPasword;
+  //Validate inputs
+  //Display password
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ref code
 
   //   function generatedPassword() {
   //     let createdPassword = [];
@@ -66,42 +124,3 @@ function generatePassword () {
   //   }
   //   console.log("my password: " + createdPassword);
   // }
-
-  var passwordCharacters = []
-  if (pwSpecial) {passwordCharacters = passwordCharacters.concat(special)}
-  if (pwNumber) {passwordCharacters = passwordCharacters.concat(number)}
-  if (pwLower) {passwordCharacters = passwordCharacters.concat(lowerCase)}
-  if (pwUpper) {passwordCharacters = passwordCharacters.concat(upperCase)}
-
-  console.log(passwordCharacters)
-
-    // Empty string to be filled based on for loop selecting random characters from the array
-    var randomPassword = ""
-    for (var i = 0; i < pwLength; i++) {
-      randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-      console.log(randomPassword)
-    }
-    return randomPassword;
-
-
-
-
-
-    generatedPassword();
-  //Validate inputs
-  //Generate password
-  //Display password
-    return "Generated Password here";
-
-}
-generatePassword();
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
